e818eeb5d87903d404983cd26656d1413a598738
Roman <regorov@neuro.net>
2024-02-19 01:23:53 +0300

    Describe a database

---
+++ config.py
@@ -0,0 +1,8 @@
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: PostgresDsn
+
+    class Config:
+        env_file = ".env"
